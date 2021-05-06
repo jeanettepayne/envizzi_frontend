685618af50313106d676813d8c4610cbf587054b
jeanettepayne <jeanetterpayne@gmail.com>
2021-05-05 21:03:16 -0400

    added getBoards function

--- development/envizzi-js-project/envizzi_frontend/index.js
+++ development/envizzi-js-project/envizzi_frontend/index.js
@@ -1,1 +1,21 @@
-console.log("in index.js!");
+const endPoint = "http://localhost:3000/api/v1/boards"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getBoards();
+})
+
+function getBoards() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(boards => {
+        boards.data.forEach(board => {
+            const boardVisual = `
+            <div data-id=${board.id}>
+                <h2>${board.attributes.title}</h2>
+            </div> `;
+
+            document.querySelector('#board-container').innerHTML += boardVisual
+        })
+    })
+}
+
