fe9ec58308dcd3d6d7687f0a88a3d10593bc0326
jeanettepayne <jeanetterpayne@gmail.com>
2021-07-27 20:15:43 -0400

    clean up

--- development/envizzi-js-project/envizzi_frontend/index.js
+++ development/envizzi-js-project/envizzi_frontend/index.js
@@ -18,34 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// function getBoards() {
-//     fetch(endPoint)
-//     .then(response => response.json())
-//     .then(boards => {
-//         boards.data.forEach(board => {
-//             const boardVisual = `
-//                 <div data-id=${board.id}>
-//                     <h2>${board.attributes.title}</h2>
-//                 </div> `);
-
-//         console.log(boardVisual);
-
-        // board.attributes.items.forEach(item => {
-        //     const itemVisual = `
-        //     <div item-id=${item.id}></div>
-        //         <p>${item.name}</p>`
-        //  })
-const itemVisual = board.data.attributes.items.forEach(item => {
-    item.name
-})
-
-// const boardVisual = `<div data-id=${board.id}>
-// <h2>${board.attributes.title}</h2>
-// board.data.attributes.items.forEach(item => {
-//     item.name
-// }) 
-// </div>`
-
 function getBoards() {
     fetch(endPoint)
     .then(response => response.json())
